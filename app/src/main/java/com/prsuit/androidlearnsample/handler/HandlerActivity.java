7ab35759c893b723653569874a1281f2410d0a62
prsuit <prsuit17@gmail.com>
2020-08-13 23:54:38 +0800

    practice Handler and HandlerThread

--- app/src/main/java/com/prsuit/androidlearnsample/handler/HandlerActivity.java
+++ app/src/main/java/com/prsuit/androidlearnsample/handler/HandlerActivity.java
@@ -21,20 +21,26 @@ public class HandlerActivity extends AppCompatActivity {
     private static final int SUB_TO_MAIN = 1;
     private static final int MAIN_TO_SUB = 2;
     private static final int SUB_TO_SUB = 3;
+    private static final int DOWNLOAD_IMAGE_IN_THREAD = 4;//子线程开始工作下载图片消息
+    private static final int LOAD_IMAGE = 5;//主线程更新UI消息
 
     public static void startAct(Context context) {
         context.startActivity(new Intent(context, HandlerActivity.class));
     }
 
-    //使用Handler自己处理消息
+    //主线程Handler重写自身的处理消息方法
     private Handler mainHandler = new Handler(){
         @Override
         public void handleMessage(@NonNull Message msg) {
 //            super.handleMessage(msg);
-            Log.e(TAG, "handleMessage: --mainHandler-->" );
             switch (msg.what){
                 case SUB_TO_MAIN:
-                    Log.e(TAG, "handleMessage: --MainHandler 接收线程：-->"+Thread.currentThread().getName() );
+                    Log.e(TAG, "handleMessage: "+ msg.what +"--MainHandler 接收线程：-->"+Thread.currentThread().getName() );
+                    break;
+                case LOAD_IMAGE:
+                    Log.e(TAG, "handleMessage: "+ msg.what +"--MainHandler 接收线程：-->"+Thread.currentThread().getName() );
+                    //主线程更新UI，加载图片
+                    //imageView.setImageBitmap((Bitmap) msg.obj);
                     break;
             }
 
@@ -42,7 +48,7 @@ public class HandlerActivity extends AppCompatActivity {
         }
     };
 
-    //使用Handler.Callback()处理消息
+    //主线程Handler使用Handler.Callback()处理消息
 //    private Handler mainHandler2 = new Handler(new Handler.Callback() {
 //        @Override
 //        public boolean handleMessage(@NonNull Message msg) {
@@ -53,6 +59,7 @@ public class HandlerActivity extends AppCompatActivity {
 
     private HandlerThread handlerThread;
     private Handler subHandler;
+    private Handler subHandlerWithCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +73,7 @@ public class HandlerActivity extends AppCompatActivity {
         handlerThread = new HandlerThread("MyHandlerThread");
         //必须开启HandlerThread子线程，在run()初始化Looper和开启loop消息
         handlerThread.start();
-        //获取子Handler 将HandlerThread与Handler绑定：利用handlerThread获取子线程的Looper，创建子线程的Handler实例 在子线程中处理消息
+        //获取子线程Handler 将HandlerThread与Handler绑定：利用handlerThread获取子线程的Looper，创建子线程的Handler实例 在子线程中处理消息
         subHandler = new Handler(handlerThread.getLooper()){
             @Override
             public void handleMessage(@NonNull Message msg) {
@@ -82,6 +89,25 @@ public class HandlerActivity extends AppCompatActivity {
                 }
             }
         };
+
+        subHandlerWithCallback = new Handler(handlerThread.getLooper(), new Handler.Callback() {
+            @Override
+            public boolean handleMessage(@NonNull Message msg) {
+                Log.e(TAG, "handleMessage: "+ msg.what+"--subHandlerWithCallback 接收线程：-->"+Thread.currentThread().getName() );
+                if (msg.what == DOWNLOAD_IMAGE_IN_THREAD){
+                    Log.e(TAG, "handleMessage: "+"--subHandlerWithCallback 收到下载图片消息--->" +"开始下载...");
+                    //在子线程中进行网络请求下载图片
+                    //Bitmap bitmap = downLoadUrl()
+                    //下载完成通知主线程去更新UI界面
+                    Message message = Message.obtain();
+                    message.what = LOAD_IMAGE;
+                    //message.obj = bitmap;
+                    Log.e(TAG, "handleMessage: --下载完成通知主线程更新UI，发送线程：-->"+Thread.currentThread().getName());
+                    mainHandler.sendMessage(message);
+                }
+                return false;
+            }
+        });
     }
 
     //在子线程中发消息给主线程处理
@@ -114,10 +140,18 @@ public class HandlerActivity extends AppCompatActivity {
         thread2.start();
     }
 
+    //在主线程中通知子线程下载图片
+    public void subThreadDownloadImageClick(View view){
+        Log.e(TAG, "subThreadDownloadImage: --发送线程：-->"+Thread.currentThread().getName());
+        subHandlerWithCallback.sendEmptyMessage(DOWNLOAD_IMAGE_IN_THREAD);
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //在不需要HandlerThread的时候需要手动停止掉
         handlerThread.quit();
+        //移除所有消息，防止内存泄露
+        mainHandler.removeCallbacksAndMessages(null);
     }
 }
