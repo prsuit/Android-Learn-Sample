4f87d74648307efeaefbed862900919edd90a5a8
prsuit <318663050@qq.com>
2021-11-06 22:15:23 +0800

    泛型、反射、代理

---
+++ app/src/main/java/com/prsuit/androidlearnsample/features/concurrent/OnlyMain.java
@@ -0,0 +1,24 @@
+package com.prsuit.androidlearnsample.features.concurrent;
+
+//import java.lang.management.ManagementFactory;
+//import java.lang.management.ThreadInfo;
+//import java.lang.management.ThreadMXBean;
+
+/**
+ *@author Mark老师   享学课堂 https://enjoy.ke.qq.com 
+ *
+ *类说明：java的多线程无处不在
+ */
+public class OnlyMain {
+    public static void main(String[] args) {
+    	/*虚拟机线程管理的接口*/
+//    	ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+		/*取得线程信息*/
+//    	ThreadInfo[] threadInfos =
+//    	 threadMXBean.dumpAllThreads(false, false);
+//    	for(ThreadInfo threadInfo:threadInfos) {
+//    		System.out.println("["+threadInfo.getThreadId()+"]"+" "
+//    				+threadInfo.getThreadName());
+//    	}
+    }
+}
---
+++ app/src/main/java/com/prsuit/androidlearnsample/features/concurrent/StartAndRun.java
@@ -0,0 +1,40 @@
+package com.prsuit.androidlearnsample.features.concurrent;
+
+
+import com.prsuit.androidlearnsample.features.concurrent.tools.SleepTools;
+
+/**
+ *@author Mark老师   享学课堂 https://enjoy.ke.qq.com 
+ *
+ *类说明：start和run方法的区别
+ */
+public class StartAndRun {
+    public static class ThreadRun extends Thread{
+
+        @Override
+        public void run() {
+            int i = 90;
+            while(i>0){
+            	SleepTools.ms(1000);
+                System.out.println("I am "+Thread.currentThread().getName()
+                		+" and now the i="+i--);
+            }
+        }
+    }
+    
+    private static class User {
+    	public void us() {
+    		
+    	}
+    }
+
+    public static void main(String[] args) {
+        ThreadRun beCalled = new ThreadRun();
+        beCalled.setName("beCalled");
+        //beCalled.run();
+
+        new User().us();
+
+        beCalled.start();
+    }
+}
